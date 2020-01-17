c5f0f74b307595043ea5f2a4c24cf5aec4f80332
nnbai <nnbai@zkwuan.com>
2020-01-17 10:41:26 +0800

    初始化项目

---
+++ pages/index/index.js
@@ -0,0 +1,75 @@
+// pages/index/index.js
+const app = getApp()
+const WXAPI = require('apifm-wxapi');
+WXAPI.init('bainana');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getArticles();//获取文章列表
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  getArticles(){
+    WXAPI.cmsArticles().then((res) =>{
+      console.log(res)
+    })
+  }
+})
